5aa2bbca2c3c7eec6ea5d6c5c808e3a4148ef15b
sumin-dev <bonar8888@naver.com>
2024-07-04 09:59:37 +0900

    [CHORE] Github Slack 연동 테스트 #1

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 
 // Button이 텍스트를 렌더링하는지 테스트
 test('renders button with text', () => {
-  render(<Button text="Click me" onClick={() => {}} />);
+  render(<Button text="Click m" onClick={() => {}} />);
   const buttonElement = screen.getByText(/Click me/i);
   expect(buttonElement).toBeInTheDocument();
 });
