a7bb937b5538f17737ea75f11e056136bf2dfbd9
sumin-dev <bonar8888@naver.com>
2024-07-07 07:58:56 +0900

    [CHORE] config 파일 alias 설정 추가 #7

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,8 @@ export default {
   moduleNameMapper: {
     '^.+\\.svg$': 'jest-svg-transformer',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^@icons/(.*)$': '<rootDir>/src/assets/icons/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
