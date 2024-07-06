dc45f9fe0c11c0b97c30e29e0d6872cac398d6ca
sumin-dev <bonar8888@naver.com>
2024-07-07 07:12:53 +0900

    [FEAT] 로그인 및 회원가입 페이지 에러 컴포넌트 구현 #7

---
+++ src/components/Auth/AuthError/AuthError.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import errorIcon from '../../../assets/icons/error.svg';
+import closeIcon from '../../../assets/icons/close.svg';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 20px;
+  width: calc(100% - 40px);
+  border: none;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.red500};
+  position: absolute;
+  bottom: 100px;
+  left: 20px;
+  z-index: 9999;
+`;
+
+const Icon = styled.img`
+  margin-right: 8px;
+`;
+
+const Text = styled.span`
+  flex-grow: 1;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 1.6rem;
+  font-family: 'NanumSquareBold';
+`;
+
+const CloseButton = styled.img`
+  cursor: pointer;
+`;
+
+interface AuthErrorProps {
+  text: string;
+  $isVisible: boolean;
+  onClose: () => void;
+}
+
+const AuthError: React.FC<AuthErrorProps> = ({ text, onClose }) => {
+  return (
+    <Wrapper>
+      <Icon src={errorIcon} alt="error icon" />
+      <Text>{text}</Text>
+      <CloseButton src={closeIcon} alt="close button" onClick={onClose} />
+    </Wrapper>
+  );
+};
+
+export default AuthError;
