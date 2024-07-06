c6d3809574fa83db8064a4d29652c7739e7b7b64
sumin-dev <bonar8888@naver.com>
2024-07-07 07:10:32 +0900

    [FIX] Button props prefix 설정 및 type 추가 #7

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -5,9 +5,9 @@ import Button from '../../components/Button/Button';
 const Home: React.FC = () => {
   return (
     <div>
-      {/* button 사용 예시 props = {size(small, medium, large) / variant(primary or null)}, children = text ※theme 참고※ */}
+      {/* button 사용 예시 props = {size(small, medium, large) / $variant(primary or null)}, children = text ※theme 참고※ */}
       <Button size="small">중복확인</Button>
-      <Button size="large" variant="primary">
+      <Button size="large" $variant="primary">
         로그인
       </Button>
     </div>
