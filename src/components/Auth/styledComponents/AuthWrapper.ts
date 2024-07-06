0a704111442d74c7a5ba1373542c2dc6d7642c1d
sumin-dev <bonar8888@naver.com>
2024-07-07 07:24:01 +0900

    [FEAT] 로그인 및 회원가입 페이지 공통 styled-components 구현 #7

---
+++ src/components/Auth/styledComponents/AuthWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const AuthWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px 30px;
+  position: relative;
+`;
