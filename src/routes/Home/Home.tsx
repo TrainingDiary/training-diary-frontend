bd2915ff4b63393a7bcfa20f0f1d0a2b268f3f7c
sumin-dev <bonar8888@naver.com>
2024-07-07 09:24:03 +0900

    [FIX] Button props 중 size에 prefix 설정 #7

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -6,8 +6,8 @@ const Home: React.FC = () => {
   return (
     <div>
       {/* button 사용 예시 props = {size(small, medium, large) / $variant(primary or null)}, children = text ※theme 참고※ */}
-      <Button size="small">중복확인</Button>
-      <Button size="large" $variant="primary">
+      <Button $size="small">중복확인</Button>
+      <Button $size="large" $variant="primary">
         로그인
       </Button>
     </div>
