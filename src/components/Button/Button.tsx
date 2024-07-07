f9c016cf63048d1851d6017b84d7917fdf61b678
sumin-dev <bonar8888@naver.com>
2024-07-08 06:30:53 +0900

    [FIX] Button이 primary가 아닐 때 active 상태에서 글자 색상 깜빡임 수정 #7

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -24,7 +24,6 @@ const ButtonWrapper = styled.button<ButtonProps>`
   &:active {
     background-color: ${({ $variant, theme }) =>
       $variant === 'primary' ? theme.colors.main700 : theme.colors.gray400};
-    color: ${(props) => props.theme.colors.white};
   }
   &:hover {
     background-color: ${({ $variant, theme }) =>
