ad9414f98047b3e548cacac7c475febabc56f7e3
sumin-dev <bonar8888@naver.com>
2024-07-08 23:07:42 +0900

    [DESIGN] AuthForm 간격 조정 #9

--- src/components/Auth/styledComponents/AuthForm.ts
+++ src/components/Auth/styledComponents/AuthForm.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const AuthForm = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 40px;
+  gap: 20px;
   width: 100%;
+
+  & > *:last-child {
+    margin-top: 20px;
+  }
 `;
