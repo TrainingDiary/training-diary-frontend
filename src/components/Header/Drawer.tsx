247bb87e0702a90e312b6fb2758b748b38c979b6
JeongMin_Byeon <bjm4256@gmail.com>
2024-07-09 12:13:10 +0900

    [FEAT] Header 개발 #4

---
+++ src/components/Header/Drawer.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import homeIcon from '@icons/home.svg';
+import appointmentIcon from '@icons/appointment.svg';
+import logOutIcon from '@icons/logout.svg';
+
+const DrawerWrapper = styled.div<{ $isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 300px;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
+  transform: ${({ $isOpen }) => ($isOpen ? 'translateX(0)' : 'translateX(100%)')};
+  transition: transform 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  z-index: 1000;
+`;
+
+const DrawerItem = styled(Link)`
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+  color: ${({ theme }) => theme.colors.gray900};
+  text-decoration: none;
+  img {
+    margin-right: 10px;
+  }
+`;
+
+interface DrawerProps {
+  $isOpen: boolean;
+  onClose: () => void;
+}
+
+const Drawer: React.FC<DrawerProps> = ({ $isOpen, onClose }) => {
+  return (
+    <DrawerWrapper $isOpen={$isOpen}>
+      <DrawerItem to="/" onClick={onClose}>
+        <img src={homeIcon} alt="Home" />
+        Home
+      </DrawerItem>
+      <DrawerItem to="/appointment" onClick={onClose}>
+        <img src={appointmentIcon} alt="Appointment" />
+        Appointment
+      </DrawerItem>
+      <DrawerItem as="div" onClick={onClose}>
+        <img src={logOutIcon} alt="Logout" />
+        Logout
+      </DrawerItem>
+    </DrawerWrapper>
+  );
+};
+
+export default Drawer;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import backIcon from '@icons/backIcon.svg';
+import bellIcon from '@icons/bell.svg';
+import hamBtnIcon from '@icons/hamBtn.svg';
+import { useLocation } from 'react-router-dom';
+import Drawer from './Drawer';
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  position: relative;
+
+  margin: 20px auto;
+  padding: 0 20px;
+
+  h1 {
+    font-size: 2.4rem;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin: 0;
+    line-height: 1;
+  }
+`;
+
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`;
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
+
+  return (
+    <>
+      <HeaderWrapper>
+        <Icon>
+          <img src={location.pathname === '' ? '' : backIcon} alt="Back" />
+        </Icon>
+        <h1>트.다</h1>
+        <Icon>
+          <img src={bellIcon} alt="Bell" />
+          <img src={hamBtnIcon} alt="Menu" onClick={toggleDrawer} />
+        </Icon>
+      </HeaderWrapper>
+      <Drawer $isOpen={isDrawerOpen} onClose={toggleDrawer} />
+    </>
+  );
 };
 
 export default Header;
