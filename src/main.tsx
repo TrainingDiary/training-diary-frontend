e1403a21f2cb12ba0f13665fb5ed329639584eeb
JeongMin_Byeon <bjm4256@gmail.com>
2024-07-12 00:16:16 +0900

    [CHORE] msw 개발 환경에서만 작동되는 조건문 삭제 #17

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from './App.tsx';
 
 // enableMocking 함수 정의
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return;
+  // }
 
   const { worker } = await import('./mocks/browser');
 
