d1cc15848ffb51b2367f97493e4fa268fcbde780
sumin-dev <bonar8888@naver.com>
2024-07-20 06:50:34 +0900

    [FEAT] RoleProtectedRoute 추가 #40

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,9 @@ import {
 } from 'react-router-dom';
 
 import ProtectedRoute from './ProtectedRoute';
-import HomeRedirect from './HomeRedirect';
 import PublicRoute from './PublicRoute';
+import RoleProtectedRoute from './RoleProtectedRoute';
+import HomeRedirect from './HomeRedirect';
 import Layout from '@components/Layout';
 import Login from '@pages/Login/Login';
 import Signup from '@pages/Signup/Signup';
@@ -16,7 +17,9 @@ import WorkOutManagement from '@pages/Trainer/WorkOutManagement';
 import TraineeHome from '@pages/Trainee/TraineeHome';
 import Dashboard from '@pages/Trainee/Dashboard';
 import Session from '@pages/Trainee/Session';
+import SessionDetail from '@pages/Trainee/SessionDetail';
 import Diet from '@pages/Trainee/Diet';
+import DietDetail from '@pages/Trainee/DietDetail';
 import AppointmentHome from '@pages/Appointment/AppointmentHome';
 import MonthlyContent from '@pages/Appointment/MonthlyContent';
 import WeeklyContent from '@pages/Appointment/WeeklyContent';
@@ -36,19 +39,25 @@ const router = createBrowserRouter([
           },
           {
             path: 'trainer',
-            element: <TrainerHome />,
+            element: <RoleProtectedRoute />,
             children: [
               {
-                index: true,
-                element: <Navigate to="trainees" />,
-              },
-              {
-                path: 'trainees',
-                element: <TraineeManagement />,
-              },
-              {
-                path: 'workouts',
-                element: <WorkOutManagement />,
+                path: '',
+                element: <TrainerHome />,
+                children: [
+                  {
+                    index: true,
+                    element: <Navigate to="trainees" />,
+                  },
+                  {
+                    path: 'trainees',
+                    element: <TraineeManagement />,
+                  },
+                  {
+                    path: 'workouts',
+                    element: <WorkOutManagement />,
+                  },
+                ],
               },
             ],
           },
@@ -69,8 +78,16 @@ const router = createBrowserRouter([
                 element: <Session />,
               },
               {
+                path: 'session/:sessionId',
+                element: <SessionDetail />,
+              },
+              {
                 path: 'diet',
                 element: <Diet />,
+              },
+              {
+                path: 'diet/:dietId',
+                element: <DietDetail />,
               },
             ],
           },
