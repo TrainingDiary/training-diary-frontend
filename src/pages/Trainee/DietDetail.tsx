d7ff927d90e9338624c21a625e6d8080d5c929ca
sumin-dev <bonar8888@naver.com>
2024-07-23 04:58:54 +0900

    [FEAT] 식단 코멘트 뷰 작업 #46

--- src/pages/Trainee/DietDetail.tsx
+++ src/pages/Trainee/DietDetail.tsx
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import threeDots from '@icons/diet/threeDots.svg';
+import commentAvatar from '@icons/diet/commentAvatar.svg';
+import activeSendComment from '@icons/diet/activeSendComment.svg';
+import inactiveSendComment from '@icons/diet/inactiveSendComment.svg';
 import { SectionWrapper } from '@components/Common/SectionWrapper';
 import Modal from '@components/Common/Modal/Modal';
+import Alert from '@components/Common/Alert/Alert';
 import { hexToRgba } from 'src/utils/hexToRgba';
 import useModals from 'src/hooks/useModals';
 
@@ -18,7 +22,7 @@ const ImgContainer = styled.div`
   position: relative;
   width: 100%;
   padding-bottom: 100%;
-  border-radius: 8px;
+  border-radius: 10px;
   overflow: hidden;
   box-shadow: 0 6px 8px ${({ theme }) => hexToRgba(theme.colors.black, 0.35)};
 
@@ -90,14 +94,139 @@ const MenuItem = styled.div`
   }
 `;
 
+const CommentContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+  width: 100%;
+`;
+
+const CommentText = styled.p`
+  flex: 1;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.colors.gray900};
+`;
+
+const CommentInput = styled.textarea`
+  flex: 1;
+  height: 60px;
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.gray400};
+  border-radius: 5px;
+  outline: none;
+  font-size: 1.4rem;
+  line-height: 1.5;
+  color: ${({ theme }) => theme.colors.gray700};
+  font-family:
+    'NanumSquare',
+    'NotoSans KR',
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+  resize: none;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  gap: 3px;
+`;
+
+const Button = styled.button`
+  padding: 2px 35px;
+  font-size: 1.6rem;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+`;
+
+const SaveButton = styled(Button)`
+  color: ${({ theme }) => theme.colors.white};
+  background: ${({ theme }) => theme.colors.main500};
+
+  &:hover,
+  &:active {
+    background: ${({ theme }) => theme.colors.main700};
+  }
+`;
+
+const CancelButton = styled(Button)`
+  color: ${({ theme }) => theme.colors.gray600};
+  background: ${({ theme }) => theme.colors.gray100};
+  border: 1px solid ${({ theme }) => theme.colors.gray200};
+
+  &:hover,
+  &:active {
+    background: ${({ theme }) => theme.colors.gray300};
+  }
+`;
+
+const SendCommentInputWrapper = styled.div<{
+  $hasContent: boolean;
+}>`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.gray200};
+  border-radius: 5px;
+  padding: 10px;
+  width: 100%;
+  background-color: ${({ theme, $hasContent }) =>
+    $hasContent ? theme.colors.white : theme.colors.gray200};
+`;
+
+const SendCommentInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  color: ${({ theme }) => theme.colors.gray700};
+  font-size: 1.4rem;
+  background-color: transparent;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray500};
+  }
+`;
+
+const SendCommentButton = styled.div<{
+  $hasContent: boolean;
+}>`
+  width: 25px;
+  height: 25px;
+  cursor: ${({ $hasContent }) => ($hasContent ? 'pointer' : 'not-allowed')};
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 const DietDetail: React.FC = () => {
   const navigate = useNavigate();
   const { traineeId } = useParams<{ traineeId: string }>();
   const { openModal, closeModal, isOpen } = useModals();
-  const [menuVisible, setMenuVisible] = useState(false);
+  const [postMenuVisible, setPostMenuVisible] = useState<boolean>(false);
+  const [commentMenuVisible, setCommentMenuVisible] = useState<boolean>(false);
+  const [editCommentId, setEditCommentId] = useState<number | null>(null);
+  const [commentText, setCommentText] = useState<string>('');
+  const [newComment, setNewComment] = useState<string>('');
+  const [errorAlert, setErrorAlert] = useState<string>('');
 
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
+  const togglePostMenu = () => {
+    setPostMenuVisible(!postMenuVisible);
+  };
+
+  const toggleCommentMenu = () => {
+    setCommentMenuVisible(!commentMenuVisible);
   };
 
   const onSaveModal = (modalName: string) => {
@@ -105,11 +234,43 @@ const DietDetail: React.FC = () => {
       // 식단 삭제 API 요청 단계 추가
       navigate(`/trainee/${traineeId}/diet`);
     } else if (modalName === 'deleteCommentModal') {
-      // 댓글 삭제 API 요청 단계 추가
+      // 댓글 삭제 API 요청 단계 추가 (refetch)
     }
 
     closeModal(modalName);
   };
+
+  const onEditComment = () => {
+    setEditCommentId(1);
+    setCommentText('조금 더 노력하면 더 좋은 결과를 얻을 수 있을 것 같습니다.');
+  };
+
+  const onCancelEdit = () => {
+    setEditCommentId(null);
+  };
+
+  const onSaveEdit = () => {
+    if (!commentText) return setErrorAlert('댓글을 입력해주세요.');
+
+    // 댓글 수정 API 요청 단계 추가
+    setEditCommentId(null);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  };
+
+  const onAddComment = () => {
+    if (!newComment) return;
+
+    // 댓글 추가 API 요청 단계 추가
+    console.log(newComment);
+    setNewComment('');
+  };
+
+  const onCloseErrorAlert = () => setErrorAlert('');
 
   return (
     <SectionWrapper>
@@ -130,11 +291,10 @@ const DietDetail: React.FC = () => {
               <br />
               다음에는 더 맛있는 걸 먹고 싶습니다.
             </Contents>
-            <DotButton onClick={toggleMenu}>
+            <DotButton onClick={togglePostMenu}>
               <img src={threeDots} alt="Button group: Delete options" />
-              {menuVisible && (
+              {postMenuVisible && (
                 <DropdownMenu>
-                  {/* <MenuItem onClick={() => alert('수정 클릭됨')}>수정</MenuItem> */}
                   <MenuItem onClick={() => openModal('deletePostModal')}>
                     삭제
                   </MenuItem>
@@ -143,6 +303,56 @@ const DietDetail: React.FC = () => {
             </DotButton>
           </ContentsBox>
         </ContentsContainer>
+        <CommentContainer>
+          <img src={commentAvatar} alt="avatar image" />
+          {editCommentId ? (
+            <React.Fragment>
+              <CommentInput
+                value={commentText}
+                onChange={e => setCommentText(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+              <ButtonContainer>
+                <SaveButton onClick={onSaveEdit}>수정</SaveButton>
+                <CancelButton onClick={onCancelEdit}>취소</CancelButton>
+              </ButtonContainer>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <CommentText>
+                조금 더 노력하면 더 좋은 결과를 얻을 수 있을 것 같습니다.
+              </CommentText>
+              <PostDate>2024. 07. 12.</PostDate>
+              <DotButton onClick={toggleCommentMenu}>
+                <img
+                  src={threeDots}
+                  alt="Button group: Edit and delete options"
+                />
+                {commentMenuVisible && (
+                  <DropdownMenu>
+                    <MenuItem onClick={onEditComment}>수정</MenuItem>
+                    <MenuItem onClick={() => openModal('deleteCommentModal')}>
+                      삭제
+                    </MenuItem>
+                  </DropdownMenu>
+                )}
+              </DotButton>
+            </React.Fragment>
+          )}
+        </CommentContainer>
+        <SendCommentInputWrapper $hasContent={!!newComment}>
+          <SendCommentInput
+            value={newComment}
+            onChange={e => setNewComment(e.target.value)}
+            placeholder="Comment"
+          />
+          <SendCommentButton onClick={onAddComment} $hasContent={!!newComment}>
+            <img
+              src={!!newComment ? activeSendComment : inactiveSendComment}
+              alt="send comment button"
+            />
+          </SendCommentButton>
+        </SendCommentInputWrapper>
         <Modal
           title="게시글 삭제"
           type="confirm"
@@ -153,6 +363,19 @@ const DietDetail: React.FC = () => {
         >
           선택한 게시글을 삭제할까요?
         </Modal>
+        <Modal
+          title="댓글 삭제"
+          type="confirm"
+          isOpen={isOpen('deleteCommentModal')}
+          onClose={() => closeModal('deleteCommentModal')}
+          onSave={() => onSaveModal('deleteCommentModal')}
+          btnConfirm="삭제"
+        >
+          선택한 댓글을 삭제할까요?
+        </Modal>
+        {errorAlert && (
+          <Alert $type="error" text={errorAlert} onClose={onCloseErrorAlert} />
+        )}
       </Wrapper>
     </SectionWrapper>
   );
