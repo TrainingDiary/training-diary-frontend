f22e035f164e5b3ca23588c85050fbb254c16b28
sumin-dev <bonar8888@naver.com>
2024-07-24 09:06:17 +0900

    [FIX] MSW 서비스워커 주석처리 #55

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 // enableMocking 함수 정의
-async function enableMocking() {
-  // if (process.env.NODE_ENV !== 'development') {
-  //   return;
-  // }
+// async function enableMocking() {
+//   // if (process.env.NODE_ENV !== 'development') {
+//   //   return;
+//   // }
 
-  const { worker } = await import('./mocks/browser');
+//   const { worker } = await import('./mocks/browser');
 
-  // `worker.start()`는 Service Worker가 요청을 가로채기 위해 준비될 때까지 대기합니다.
-  return worker.start();
-}
+//   // `worker.start()`는 Service Worker가 요청을 가로채기 위해 준비될 때까지 대기합니다.
+//   return worker.start();
+// }
 
 // enableMocking().then(() => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
