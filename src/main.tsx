6596c54689f0f3ebfd49f309ec78a2d42f242e39
JeongMin_Byeon <bjm4256@gmail.com>
2024-07-25 22:34:15 +0900

    [CHORE] msw 임시 해제 #68

--- src/main.tsx
+++ src/main.tsx
@@ -8,24 +8,24 @@ import registerServiceWorker from './firebase/registerServiceWorker.ts';
 const queryClient = new QueryClient();
 
 // enableMocking 함수 정의
-async function enableMocking() {
-  // if (process.env.NODE_ENV !== 'development') {
-  //   return;
-  // }
+// async function enableMocking() {
+// if (process.env.NODE_ENV !== 'development') {
+//   return;
+// }
 
-  const { worker } = await import('./mocks/browser');
+// const { worker } = await import('./mocks/browser');
 
-  // `worker.start()`는 Service Worker가 요청을 가로채기 위해 준비될 때까지 대기합니다.
-  return worker.start();
-}
+// `worker.start()`는 Service Worker가 요청을 가로채기 위해 준비될 때까지 대기합니다.
+//   return worker.start();
+// }
 
 registerServiceWorker();
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </React.StrictMode>
-  );
-});
+// enableMocking().then(() => {
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+// });
