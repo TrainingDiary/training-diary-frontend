a5331da7c0327423fac639ac4184c452632a1d6d
sumin-dev <bonar8888@naver.com>
2024-07-26 18:57:53 +0900

    [CHORE] 필요없는 주석 삭제 #63

--- src/firebase/registerServiceWorker.ts
+++ src/firebase/registerServiceWorker.ts
@@ -5,12 +5,6 @@ const registerServiceWorker = () => {
         .register('/firebase-messaging-sw.js', {
           scope: '/firebase-cloud-messaging-push-scope',
         })
-        .then(function (registration) {
-          console.log(
-            'Service Worker가 scope에 등록되었습니다.:',
-            registration.scope
-          );
-        })
         .catch(function (err) {
           console.log('Service Worker 등록 실패:', err);
         });
