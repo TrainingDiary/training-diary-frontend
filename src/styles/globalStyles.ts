f45c3fb45d768e12fb0964f8bcce7d48807fad9d
sumin-dev <bonar8888@naver.com>
2024-07-28 05:17:20 +0900

    [FIX] 기기별 vh 기준이 다르므로 dvh 사용 #64

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -12,13 +12,13 @@ ${reset};
 
 
 
-//font-size base = 10px = 1rem / ex) 16px === 1.6rem 
+//font-size base = 10px = 1rem / ex) 16px === 1.6rem
 html, body{
   font-size: 10px;
   line-height: 1.5;
   font-family: 'NanumSquare', 'Noto Sans KR', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
-  height: 100vh;
+  height: 100dvh;
   background-color: ${({ theme }) => theme.colors.gray200};
 }
 
